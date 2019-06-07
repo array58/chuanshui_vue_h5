775de09e06c61c9e673cb77d795d92fe05439c53
Array58 <17758033639@163.com>
2019-06-07 13:31:12 +0800

    添加项目

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="app">
+    <h1>{{test}}</h1>
+  <!-- <iframe src="../public/test.html" frameborder="0"></iframe> -->
+   
+  </div>
+</template>
+
+<script>
+ 
+export default {
+  name: 'app',
+  data(){
+    return {
+      test:'test h5',
+      t:0
+    }
+  },
+  mounted(){
+    
+    let iframe = document.createElement('iframe');
+    iframe.src = '../static/test.html'
+    document.body.appendChild(iframe)
+  }
+}
+</script>
+
+<style lang='scss'>
+#app {
+  
+}
+</style>
